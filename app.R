7fa1edb5ab5661d43a62d40a38de21b6e0ca3957
nigradwohl <nico.gradwohl@uni.kn>
2023-04-26 19:58:09 +0200

    add more graph measures

--- app.R
+++ app.R
@@ -32,20 +32,26 @@ ui <- fluidPage(
             sliderInput("n",
                         "n(nodes):",
                         min = 2,
-                        max = 100,
+                        max = 50,
+                        step = 1,
                         value = 15),
             
             sliderInput("p_rewire",
                         "p(rewire):",
                         min = 0,
                         max = 1,
+                        step = 0.1,
                         value = 0),
             
             sliderInput("p_a",
                         "Proportion petrol:",
                         min = 0,
                         max = 1,
-                        value = 0)
+                        step = 0.1,
+                        value = 0),
+            
+            checkboxInput("show_nodelabs",
+                          "Knotenlabels zeigen")
         ),
 
         # Show a plot of the generated distribution
@@ -66,16 +72,15 @@ server <- function(input, output) {
         # Preferrential attachment:
         graph_pa <- sample_pa(n = 30, power = 2, directed = FALSE)
         
+        g_rewire <- graph_pa
         
         # Adding centralities:
         # TODO
-            degree_distribution(graph_reg)
+            # degree_distribution(g_rewire)
             
-            betweenness(graph_reg)
-            eigen_centrality(graph_pa)
+            # betweenness(g_rewire)  # Show this distribution?
             
-        # Adding asssortment:
-            # TODO
+
             
             # Development area! ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             pcol_a <- 0.5  # probability of a-nodes.
@@ -86,22 +91,31 @@ server <- function(input, output) {
             # Determine colors randomly:
             rancols <- c(rep("a", n_a), rep("b", n - n_a))
             
-            V(graph_reg)$color <- rancols
+            V(g_rewire)$color <- rancols
             colvec <- c(a = unname(Petrol), b = unname(Peach))
             
-            assortativity(graph_reg, types1 = as.numeric(V(graph_reg)$color == "a"))
+            gr_ass <- assortativity(g_rewire, types1 = as.numeric(V(g_rewire)$color == "a"))
+            
+            betweenness(g_rewire) 
+            
+            V(g_rewire)$degree <- degree(g_rewire)
 
             
-            ggplot(ggnetwork(graph_reg, layout = layout.circle(graph_reg)),
+            ggplot(ggnetwork(g_rewire, layout = layout.circle(g_rewire)),
                    aes(x = x, y = y, xend = xend, yend = yend)) +
                 geom_edges(color = "darkgrey", size = 1
                            # arrow = arrow(length = unit(8, "pt"), type = "closed")
                 ) +
                 geom_nodes(aes(color = color, fill = color), size = 10) +
+                # geom_nodetext_repel(aes(label = degree)) +
+                geom_nodetext(aes(label = paste0("Grad=", degree)),
+                              nudge_x = 0.05, nudge_y = -0.01) +
                 scale_color_manual(values = colvec) +
                 scale_fill_manual(values = colvec) +
                 guides(color = "none", fill = "none") +
-                theme_blank()
+                labs(caption = paste0("Assortativität = ", sprintf("%.2f", round(gr_ass, 2)))) +
+                theme_blank() +
+                theme(plot.caption = element_text(size = 12))
             
             
             # eof. dev ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -125,7 +139,8 @@ server <- function(input, output) {
                     "Ring lattice" = make_chordal_ring(n = 15,  # input$n,
                                                        matrix(rep(3, 3), nr = 1)),
                     "Preferential attachment" = sample_pa(n = n_nodes, 
-                                                          power = 3, directed = FALSE))
+                                                          power = 3, directed = FALSE),
+                    "Fully connected" = make_full_graph(n = n_nodes))
         
 
         
@@ -147,39 +162,67 @@ server <- function(input, output) {
         V(g_rewire)$label <- round(betweenness(g_rewire), 2)
         
         
-        # Determine node types: -------------------------
+        # NODE FEATUERS: --------------------------------
+            n_nodes <- length(V(g_rewire))
+            # Determine node types: -------------------------
+                
+                n_a <- ceiling(n_nodes * input$p_a)  # get requested type a.
             
-            n_a <- ceiling(n_nodes * input$p_a)  # get requested type a.
-        
-            # Determine colors randomly:
-            rancols <- c(rep("a", n_a), rep("b", n_nodes - n_a))
+                # Determine colors randomly:
+                rancols <- c(rep("a", n_a), rep("b", n_nodes - n_a))
+                
+                V(g_rewire)$color <- rancols
+                colvec <- c(a = unname(Petrol), b = unname(Peach))
+                
+            # Centrality on each node: ----------------------
+                V(g_rewire)$degree <- degree(g_rewire)
+                V(g_rewire)$between <- betweenness(g_rewire)
+                V(g_rewire)$eigen <- eigen_centrality(g_rewire)$vector
             
-            V(g_rewire)$color <- rancols
-            colvec <- c(a = unname(Petrol), b = unname(Peach))
+        # GRAPH-LEVEL FAETUERS: -------------------------
+            # Determine assortment: -------------------------
+                gr_ass <- assortativity(g_rewire, types1 = as.numeric(V(g_rewire)$color == "a"))
+        
             
-        
-        # Determine assortment: -------------------------
-            gr_ass <- assortativity(g_rewire, types1 = as.numeric(V(g_rewire)$color == "a"))
-        
-        
+                gr_dia <- diameter(g_rewire)
+
+            
         
         # Plot: -----------------------------------------
         # plot(g_rewire, layout = g_lay,
         #      vertex.label = V(g_rewire)$label,
         #      vertex.size = 25)
         
-            ggplot(ggnetwork(g_rewire, layout = layout.circle(g_rewire)),
+            cur_graph <- ggplot(ggnetwork(g_rewire, layout = g_lay),
                    aes(x = x, y = y, xend = xend, yend = yend)) +
                 geom_edges(color = "darkgrey", size = 1
                            # arrow = arrow(length = unit(8, "pt"), type = "closed")
                 ) +
                 geom_nodes(aes(color = color, fill = color), size = 10) +
+                # geom_nodetext_repel(aes(label = degree)) +
                 scale_color_manual(values = colvec) +
                 scale_fill_manual(values = colvec) +
                 guides(color = "none", fill = "none") +
-                labs(caption = paste0("Assortativität = ", sprintf("%.2f", round(gr_ass, 2)))) +
-                theme_blank()
-        
+                labs(caption = paste0("Assortativität = ", sprintf("%.2f", round(gr_ass, 2)),
+                                      "\n",
+                                      "Durchmesser =", gr_dia)
+                     ) +
+                theme_blank() +
+                theme(plot.caption = element_text(size = 12))
+                
+                if(input$show_nodelabs){
+                    cur_graph + 
+                    geom_nodetext(aes(label = paste0("Grad=", degree, 
+                                                     "\n", 
+                                                     "Zws=", sprintf("%.2f", round(between)),
+                                                     "\n",
+                                                     "Eig=", sprintf("%.2f", round(eigen)))),
+                                  hjust = 0,
+                                  nudge_x = 0.01, nudge_y = -0.01,
+                                  size = 3)
+                } else{
+                    cur_graph
+                }       
         
 
         
