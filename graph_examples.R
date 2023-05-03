d0747302f3d3c9553a6bc9d384f3b4af4c010d1e
nigradwohl <nico.gradwohl@uni.kn>
2023-05-03 18:12:08 +0200

    add modelling examples

--- graph_examples.R
+++ graph_examples.R
@@ -146,12 +146,192 @@ library(igraph)
   # Add "data":
   dat$data <- ifelse(dat$mude < 0,
                      3,
-                     exp(dat$mude))
+                     7) + rnorm(nrow(dat))
   
   ggplot(data = dat, aes(x = mude, y = doof)) +
+    geom_point(aes(y = data), size = 4, color = "grey") +
     geom_line(size = 2, color = "black", alpha = 0.8) +
     theme_classic() +
     labs(x = "Müde", y = "Doof") +
     ylim(0, 10) +
     theme(text = element_text(size = 18))
+
   
+# Modelle: ----- 
+  
+  library(deSolve) # using the "ode" function
+  
+  sir_equations <- function(time, variables, parameters) {
+    with(as.list(c(variables, parameters)), {
+      dS <- -beta * I * S
+      dI <-  beta * I * S - gamma * I
+      dR <-  gamma * I
+      return(list(c(dS, dI, dR)))
+    })
+  }
+  
+  parameters_values <- c(
+    beta  = 0.01, # infectious contact rate (/person/day)
+    gamma = 0.5    # recovery rate (/day)
+  )
+  
+  initial_values <- c(
+    S = 95,  # number of susceptibles at time = 0
+    I =   5,  # number of infectious at time = 0
+    R =   0   # number of recovered (and immune) at time = 0
+  )
+  
+  time_values <- seq(0, 30) # days
+
+  
+  sir_values_1 <- ode(
+    y = initial_values,
+    times = time_values,
+    func = sir_equations,
+    parms = parameters_values 
+  )  
+
+  
+  sir_values_1 <- as.data.frame(sir_values_1)
+  names(sir_values_1)[-1] <- paste0("n_", names(sir_values_1)[-1])
+  
+  sirdat <- reshape(sir_values_1, direction = "long", idvar = "time", sep = "_",
+          varying = names(sir_values_1)[-1],
+          timevar = "category")
+  
+  sirdat$category <- factor(sirdat$category,
+                            levels = c("S", "I", "R"))
+
+  
+  ggplot(data = sirdat, aes(x = time, y = n, color = category)) +
+    geom_line(size = 2) +
+    scale_color_viridis_d() +
+    theme_classic() +
+    labs(x = "Zeit", y = "Anzahl", color = "Kategorie") +
+    theme(text = element_text(size = 18))
+  
+# Alternative: Frequency-based social learning ---------------
+  ################################################################
+  ## Individual learning (delta rule) 
+  ################################################################
+  
+  # Function to update the vector of option weights:
+  update_A <- function(A_it = c(0, 0),
+                       pi_it,  # payoff in round t for each option.
+                       phi
+  ){
+    
+    # Get the new option weight:
+    opWeight <- (1 - phi) * A_it + phi * pi_it
+    
+    return(opWeight)
+  }
+  
+  ################################################################
+  ## Frequency-based learning (McElreath)
+  ################################################################
+  
+  freqbias <- function(iprobs, soc_weight, n_freq, freq_dep = 1){
+    
+    p_freq <- n_freq^freq_dep / rowSums(n_freq^freq_dep)
+    
+    # TODO: We are more interested in negative frequency dependence.
+    
+    
+    p <- (1 - soc_weight) * iprobs + soc_weight * p_freq
+    
+    return(p)
+    
+  }
+  
+  softmax <- function(vals, temp){
+    evals <- exp(vals * temp)
+    
+    return(evals/sum(evals))
+  }
+  
+  
+  # Example: -------------
+  datsoc <- expand.grid(n = 0:10, freq_dep = c(0, 1, 2, 10))
+  
+  datsoc$p_a <- NA
+  
+  for(fdep in unique(datsoc$freq_dep)){
+    
+    n <- datsoc$n[datsoc$freq_dep == fdep]
+    ns <- cbind(n, max(n) - n)
+    
+    datsoc$p_a[datsoc$freq_dep == fdep] <-
+      freqbias(0, soc_weight = 1,  freq_dep = fdep,
+               n_freq = ns)[,1]
+  }
+
+  
+
+  ggplot(data = datsoc, aes(x = n, y = p_a, color = factor(freq_dep))) +
+    geom_line(size = 2) +
+    scale_color_viridis_d() +
+    theme_classic() +
+    labs(x = "Anzahl in Option A", 
+         y = "Wahrscheinlichkeit A gewählt", 
+         color = "Non-linearität") +
+    theme(text = element_text(size = 18))
+  
+  
+  ggplot(data = datsoc[datsoc$freq_dep == 2,], aes(x = n, y = p_a, color = factor(freq_dep))) +
+    geom_line(size = 2) +
+    scale_color_viridis_d() +
+    theme_classic() +
+    labs(x = "Anzahl in Option A", 
+         y = "Wahrscheinlichkeit A gewählt", 
+         color = "Non-linearität") +
+    theme(text = element_text(size = 18))
+  
+  
+  # Add data:
+  data_maj <- data.frame(n = rep(0:10, each = 10),
+                         freq_dep = NA)
+  
+  data_maj$logits = log(ifelse(data_maj$n < 5,
+                            0.01/(1-0.01), 0.99/(1-0.99))) + rlogis(nrow(data_maj), scale = 1)
+  
+  ggplot(data = datsoc[datsoc$freq_dep == 2,], aes(x = n, y = p_a, color = factor(freq_dep))
+         ) +
+    geom_point(data = data_maj, aes(y = 1/(1 + exp(-logits))),
+               color = "grey") +
+    geom_line(size = 2) +
+    scale_color_viridis_d() +
+    theme_classic() +
+    labs(x = "Anzahl in Option A", 
+         y = "Wahrscheinlichkeit A gewählt", 
+         color = "Non-linearität") +
+    theme(text = element_text(size = 18))
+  
+  
+  # Add step function:
+  ggplot(data = datsoc[datsoc$freq_dep %in% c(2, 10),], aes(x = n, y = p_a, color = factor(freq_dep))
+  ) +
+    geom_point(data = data_maj, aes(y = 1/(1 + exp(-logits))),
+               color = "grey") +
+    geom_line(size = 2) +
+    scale_color_viridis_d() +
+    theme_classic() +
+    labs(x = "Anzahl in Option A", 
+         y = "Wahrscheinlichkeit A gewählt", 
+         color = "Non-linearität") +
+    theme(text = element_text(size = 18))
+  
+  
+  ggplot(data = datsoc, aes(x = n, y = p_a, color = factor(freq_dep))) +
+    geom_point(data = data_maj, aes(y = 1/(1 + exp(-logits))),
+               color = "grey") +
+    geom_line(size = 2) +
+    scale_color_viridis_d() +
+    theme_classic() +
+    labs(x = "Anzahl in Option A", 
+         y = "Wahrscheinlichkeit A gewählt", 
+         color = "Non-linearität") +
+    theme(text = element_text(size = 18))
+  
+# DeGroot process:
+  groot_dat <- 
